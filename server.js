6e03f339c21d3dceac6ac5bf3a21962c950e93eb
sangam verma <sangamverma955911@gmail.com>
2023-11-01 19:40:18 +0530

    cors2

--- server.js
+++ server.js
@@ -13,6 +13,34 @@ app.use(cors({ origin: "*" }));
 // app.use(cors({ origin: "https://ecommerce-sangam.netlify.app" }));
 //------------
 
+//-------------
+
+// Add headers
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+//---------------
+
 //uncaught error ** such as you type console.log(sangam) but sangam not defied 
 //so we want to close server at upper so here we define upper
 process.on("uncaughtException", (err) => {
