edb8581df2ead00d6e83b3a60358334ee47bfef1
sangam verma <sangamverma955911@gmail.com>
2023-11-03 08:26:14 +0530

    cors lord

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,10 +1,12 @@
 const sendToken = (user, statuscode, res) => {
   const token = user.generatejwtToken();
   const options = {
-    httpOnly: true,
     expires: new Date(
       Date.now() + process.env.COKKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
+    secure: true, // set to true if your using https or samesite is none
+    httpOnly: true, // backend only
+    sameSite: "none", // set to none for cross-request
   };
 
   res.status(statuscode).cookie("token", token, options).json({
