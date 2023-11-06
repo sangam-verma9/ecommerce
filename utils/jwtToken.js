f2847bfb4de7130521705dcab5211ab373cdf119
sangam verma <sangamverma955911@gmail.com>
2023-11-06 21:49:39 +0530

    httponly

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statuscode, res) => {
       Date.now() + process.env.COKKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     secure: true, // set to true if your using https or samesite is none
-    httpOnly: true, // backend only
+    // httpOnly: true, // backend only
     sameSite: "none", // set to none for cross-request
   };
 
