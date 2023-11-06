5fbf3682e25a77983bfd5a8c7985771063887327
sangam verma <sangamverma955911@gmail.com>
2023-11-06 22:16:40 +0530

    universel cookie

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,3 +1,6 @@
+const Cookies = require("universal-cookie");
+const cookies = new Cookies();
+
 const sendToken = (user, statuscode, res) => {
   const token = user.generatejwtToken();
   const options = {
@@ -5,9 +8,11 @@ const sendToken = (user, statuscode, res) => {
       Date.now() + process.env.COKKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     secure: true, // set to true if your using https or samesite is none
-    // httpOnly: true, // backend only
+    httpOnly: true, // backend only
     sameSite: "none", // set to none for cross-request
   };
+
+  cookies.set("token", token, options);
 
   res.status(statuscode).cookie("token", token, options).json({
     success: true,
