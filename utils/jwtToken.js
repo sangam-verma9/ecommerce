73604ede740d0f786c38d9e76c844466348bf934
sangam verma <sangamverma955911@gmail.com>
2023-11-14 18:33:24 +0530

    expires: new Date(       Dat.now() + process.env.COKKIE_EXPIRE * 24 * 60 * 60 * 1000     )

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,17 +10,7 @@ const sendToken = (user, statuscode, res) => {
     path: "/",
     // domain: ".onrender.com",
   };
-  res.set_cookie(
-    key = "token",
-    value = token,
-    secure= true, // set to true if your using https or samesite is none
-    httpOnly= true, // backend only
-    sameSite= "lax", // set to none for cross-request
-    path= "/",
-    expires= new Date(
-      Date.now() + process.env.COKKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-  );
+
   res.status(statuscode).cookie("token", token, options).json({
     success: true,
     user,
